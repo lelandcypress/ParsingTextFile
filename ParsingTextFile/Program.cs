bcf26832978d5735640045399058aab2f680074d
soute <souterm1@gmail.com>
2023-10-05 21:30:17 -0400

    Add project files.

---
+++ ParsingTextFile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace OutputReader
+{
+    class parsefile
+    {
+
+        public static void Main(string[] args)
+        {
+            string path = @"C:\Users\soute\Documents\Fake_Output_Data.txt";
+
+            if (File.Exists(path))
+            {
+                Console.WriteLine("YIPPEE");
+            }
+            else
+            {
+                Console.WriteLine("OOOOH NOOOOOO");
+            }
+        }
+    }
+
+}
