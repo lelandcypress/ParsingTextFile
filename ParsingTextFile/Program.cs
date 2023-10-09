815b6b54733f859f4e35e12734850c65b62e2e43
Michael Souter <souterm1@gmail.com>
2023-10-08 20:10:50 -0400

    added functionality to read target file using StreamReader library

--- ParsingTextFile/Program.cs
+++ ParsingTextFile/Program.cs
@@ -6,18 +6,57 @@ namespace OutputReader
 {
     class parsefile
     {
+       
+        static void readFile( string FilePath)
+        {
+
+            try
+            {
+
+                using (StreamReader read = new StreamReader(FilePath))
+                {
+                    //Console.WriteLine(read.ReadToEnd()); //Only use for shorter blocks since it reads the whole output at once, can cause issues with large files//
+
+                    string line = "";
+                    while((line=read.ReadLine()) !=null) 
+                    {
+                        Console.WriteLine(line); 
+
+                    }
+                    read.Close();
+
+
+                   /* int charCode;
+                    while ((charCode = read.Read()) != -1)
+                    {
+                        char c = (char)charCode;
+                        //Console.Write(c);
+                    }*/
+                    read.Close();
+
+                }
+
+            }
+
+
+
+            catch (Exception ex) {
+                Console.WriteLine("OOOPS " + ex.Message);
+            }
+        }
 
         public static void Main(string[] args)
         {
             string path = @"C:\Users\soute\Documents\Fake_Output_Data.txt";
+     
 
             if (File.Exists(path))
             {
-                Console.WriteLine("YIPPEE");
+                readFile(path);
             }
             else
             {
-                Console.WriteLine("OOOOH NOOOOOO");
+                Console.WriteLine("no matching file found");
             }
         }
     }
