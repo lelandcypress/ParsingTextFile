98b2d933cecbb8917dfcc839a7072eecefc7cee7
Michael Souter <souterm1@gmail.com>
2023-10-10 21:45:24 -0400

    Added Regex to strip square brackets from header row of results report.

--- ParsingTextFile/Program.cs
+++ ParsingTextFile/Program.cs
@@ -2,42 +2,39 @@
 using System.IO;
 using System.Text;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
-namespace OutputReader
+string path = @"C:\Users\soute\Documents\Fake_Output_Data.txt";
+List<string> OutputFile = new List<string>();
+string resultsHeader = "RESULT_DATA";
+
+try
 {
-    class parsefile
+    //save whole file into a list 
+    using (StreamReader read = new StreamReader(path))
     {
-        static void Main(string[] args)
-       
+        string line = "";
+        while ((line = read.ReadLine()) != null)
         {
-            string path = @"C:\Users\soute\Documents\Fake_Output_Data.txt";
-            List<string> OutputFile = new List<string>();
-            string resultsHeader = "RESULT_DATA";
+            OutputFile.Add(line);
 
-            try
-            {
-                using (StreamReader read = new StreamReader(path))
-                {
-                    string line = "";
-                    while((line=read.ReadLine()) !=null) 
-                    {
-                        OutputFile.Add(line);
-
-                    }
-                    read.Close();
-                }
-
-                int resultsHeaderIndx = OutputFile.FindIndex(i => i.Contains(resultsHeader));
-
-
-            }
-
-            catch (Exception ex) {
-                Console.WriteLine("OOOPS " + ex.Message);
-            }
         }
-
-        
+        read.Close();
     }
+    //searches through list and finds "RESULTS_DATA" since this will be how we break out the two files
+    int resultsHeaderIndx = OutputFile.FindIndex(i => i.Contains(resultsHeader));
+    if (resultsHeaderIndx >= 0)
+    {
+        List<string> headerReportRaw = OutputFile.GetRange(0, resultsHeaderIndx - 1);
+        List<string> resultsReportRaw = OutputFile.GetRange(resultsHeaderIndx, OutputFile.Count - resultsHeaderIndx - 1);
+        resultsReportRaw[1] = Regex.Replace(resultsReportRaw[1], @"\[|\]", "");
+        Console.WriteLine(resultsReportRaw[1]);
 
+    }
+    
+}
+
+catch (Exception ex)
+{
+    Console.WriteLine("OOOPS " + ex.Message);
 }
